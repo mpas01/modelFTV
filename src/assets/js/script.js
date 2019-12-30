7b3a12cfcd369cda3d56fce31ead37ce91a89618
marlonShawDEV <marlon_shaw@freddiemac.com>
2019-12-30 12:42:50 -0500

    test data types

---
+++ src/assets/js/script.js
@@ -0,0 +1,23 @@
+/*
+//string
+var firstName = 'John';
+console.log(firstName);
+var lastName = 'Smith';
+
+//number
+var age = 28;
+
+//boolean
+var fullAge = true;
+console.log(fullAge);
+
+// undefined var w/o assignment 
+var job;
+console.log(job);
+
+//assign job a value
+job = 'Teacher';
+console.log(job);
+*/
+
+/* **** TYPE COERCION **** */
