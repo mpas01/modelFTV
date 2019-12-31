018411d0c3f6016c4d422db1f24469faee9bc4c8
marlonShawDev <marlon_shaw@freddiemac.com>
2019-12-31 14:38:39 -0500

    type coercion

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -20,4 +20,36 @@ job = 'Teacher';
 console.log(job);
 */
 
-/* **** TYPE COERCION **** */
+/*
+
+**** TYPE COERCION ****
+
+var firstName = 'John';
+var age = 28;
+var job = 'teacher';
+var isMarried 
+  isMarried = false;
+
+//console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+ */
+
+/* 
+**** TYPE Mutation **** 
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+prompt('What is his last name?');
+// You can also store the users input from the prompt function into a variable.
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/* 
+**** Basic Operators ****  */
+//Determine John's age by subtracting his age from the current year.
+
+var yearJohn = 2020 - 32;
